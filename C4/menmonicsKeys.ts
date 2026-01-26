c937cbee11eea55713e1a79e15a87f93427d3934
suppamann <er.yellowflash@gmail.com>
2026-01-27 01:03:59 +0530

    after git rm

--- C4/menmonicsKeys.ts
+++ C4/menmonicsKeys.ts
@@ -11,5 +11,5 @@ for (let i = 0; i < 4; i++) {
     const path = `m/44'/501'/${i}'/0'`; // derivation path
     const derivedSeed = derivePath(path, seed.toString("hex")).key;    
     const secret = nacl.sign.keyPair.fromSeed(derivedSeed).secretKey;
-    console.log(Keypair.fromSecretKey(secret).publicKey.toBase58());   
+    console.log(Keypair.fromSecretKey(secret).publicKey.toBase58());
 }
